7092e06bb5aed4f2d15a60009e0d723338b0a1f4
undight <udannn2771@gmail.com>
2021-12-29 16:23:23 -0700

    first commit

---
+++ cratefarm.lua
@@ -0,0 +1,34 @@
+local active = false
+local player = game.Players.LocalPlayer
+local humanoid  = player.Character.HumanoidRootPart
+local tycoon = player.PlayerTycoon.Value
+
+local input = game:GetService("UserInputService")
+local function onInputBegan(inp, gameProcessed)
+    if inp:GetFocusedTextBox() == nil then
+        if inp.UserInputType == Enum.UserInputType.Keyboard then
+            if inp.KeyCode == Enum.KeyCode.U then
+                active = not active
+                print(active)
+            end
+        end
+    end
+end
+input.InputBegan:Connect(onInputBegan)
+
+game:GetService("Workspace").ChildAdded:Connect(function(child)
+    for _, v in pairs(game:GetService("Workspace"):GetDescendants()) do
+        if active == true then
+            if v.Name == "Open" and v:IsA("Sound") then
+                humanoid.CFrame = v.CFrame
+                task.wait(0.7)
+                humanoid.CFrame = tycoon["Base"].CFrame * CFrame.new(0, 10, 0)
+                task.wait(0.8)
+            end
+        end
+    end
+end)
+
+for _, v in pairs(getconnections(player.Idled)) do
+    v:Disable()
+end
