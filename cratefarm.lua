6298c2b25a20f679bd853622f6affb06c24647a4
undight <96847129+undight@users.noreply.github.com>
2022-01-05 23:47:01 -0700

    Update cratefarm.lua

--- cratefarm.lua
+++ cratefarm.lua
@@ -1,3 +1,6 @@
+-- Script for Miner's Haven / https://www.roblox.com/games/258258996/SHINY-Miners-Haven
+-- Press U to toggle the crate farm
+
 local active = false
 local player = game.Players.LocalPlayer
 local humanoid  = player.Character.HumanoidRootPart
